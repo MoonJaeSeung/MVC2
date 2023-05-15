adf627310ae3d3e85e586d3ce7af30af8c6aefc9
MoonJaeSeung <wotmdskfk32@naver.com>
2023-05-15 20:08:50 +0900

    spring data jpa 적용

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity //JPA가 사용하는 객체라는 뜻
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //Id는 테이블의 PK와 해당 필드를 매핑한다.
     private Long id;
 
+    @Column(name="item_name", length= 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
